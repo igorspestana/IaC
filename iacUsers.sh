4d936ac5029d4e7c5b82982c3719e7510fb0c405
Igor Pestana <79885282+igorspestana@users.noreply.github.com>
2022-08-23 16:37:27 -0300

    Upload do script

---
+++ iacUsers.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#=========================================================================
+#                                                                       
+# iacUsers.sh - Script que provisiona usuários
+#                                                                       
+# Autor: Igor Pestana (igorspestana@gmail.com)
+# Data Criação: 23/08/2022
+#                                                                       
+# Descrição: Provisiona usuários, grupos e define as permissões
+#            dos diretórios			                                       
+#                                                                       
+# Exemplo de uso: ./iacUsers.sh                                
+#                                                                       
+#=========================================================================
+
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Usuários criados com sucesso"
+
